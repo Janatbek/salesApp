33389ec34bb7c47ba7e44769cbdbc68b65ad2208
Rocky <rockygibson@gmail.com>
2015-03-06 04:46:10 -0700

    progress

---
+++ js/app.js
@@ -0,0 +1,11 @@
+var app = angular.module('oppApp', ['firebase', 'ngRoute']);
+
+app.config(function($routeProvider){
+
+	$routeProvider
+		.when('', {
+			templateUrl: '',
+			controller: ''
+
+		})
+})
